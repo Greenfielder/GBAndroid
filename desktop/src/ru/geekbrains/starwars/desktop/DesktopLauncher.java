4de3088af3b9404c95cd118f8ca7cf81d6b11a99
espinage <dimon-sad@mail.ru>
2018-10-19 01:41:40 +0300

    ДЗ 1

---
+++ desktop/src/ru/geekbrains/starwars/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.starwars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.starwars.StarWars;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 900;
+		config.height = 400;
+		new LwjglApplication(new StarWars(), config);
+	}
+}
